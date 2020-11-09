a6315d4533779599964284be1b4cd812d4ad7f3c
Ben Robinson <robinson.dri@gmail.com>
2020-11-08 19:19:08 -0800

    updating functions

--- Develop/app.js
+++ Develop/app.js
@@ -75,29 +75,37 @@ function addEmployee(){
         }
 
     ]).then(function(response){
-        console.log(response.role);
-        console.log(response.continue);
+        // console.log(response.role);
+        // console.log(response.continue);
 
-            if (response.role === "Manager") {
-            let manager = new Manager(response.name, response.id, response.email, response.officeNumber);            
+        function buildMGR(){
+            let manager = new Manager(response.name, response.id, response.emial, response.officeNumber);
             employeeArr.push(manager);
-            
-
-        }   if(response.role === "Engineer") {
-            let engineer = new Engineer(response.name, response.id, response.email, response.github);
-            employeeArr.push(engineer);
-            
         }
 
-            if(response.role === "Intern") {
-            let intern = new Intern(response.name, response.id, response.email, response.school);
-            employeeArr.push(intern);
-            
-            }
+        function buildENG() {
+            let engineer = new Engineer(response.name, response.id, response.email, response.github);
+            employeeArr.push(engineer);
+        }
 
-            if(response.continue === "yes") {
-                addEmployee();
-            } else 
+        function buildINT() {
+            let intern = new Intern(response.name,
+                response.id, response.email, response.school);
+                employeeArr.push(intern);
+        }
+
+        if (response.role === "Manager"){
+            buildMGR();
+        }
+        if (response.role === "Engineer") {
+            buildENG();
+        }
+        if (response.role === "Intern") {
+            buildINT();
+        }
+        if (response.continue === "yes") {
+            addEmployee();
+        } else       
                     
         fs.writeFile(outputPath, render(employeeArr), function(err){
             if (err){
@@ -113,25 +121,4 @@ addEmployee();
 
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
